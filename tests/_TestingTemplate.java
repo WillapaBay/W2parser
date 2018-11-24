d6eaaad6131f183c415397e905a7984a6efc28f2
WillapaBay <45308822+WillapaBay@users.noreply.github.com>
2018-11-24 13:54:34 -0800

    Add files via upload

---
+++ tests/_TestingTemplate.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import w2parser.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _TestingTemplate {
+
+
+    @Test
+    public void Test1() throws FileNotFoundException {
+    }
+}
