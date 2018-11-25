e7d6a8074fa0f4914a96ae1e1d9054faa6b8c5c3
WillapaBay <todd.steissberg@gmail.com>
2018-11-24 22:23:00 -0800

    Updated ActiveDerivedConstituentCard to handle cards with data that wrap to additional lines, such as in the Grand Coulee model. More work is needed to fix this.

--- src/w2parser/ActiveDerivedConstituentsCard.java
+++ src/w2parser/ActiveDerivedConstituentsCard.java
@@ -10,13 +10,14 @@ import java.util.List;
  * There is one line per constituent, with a value for each waterbody.
  */
 public class ActiveDerivedConstituentsCard extends Card {
-    List<String> constituentNames; // Constituent names
-    List<List<String>> Data;     // State of each constituent (ON or OFF)
-    int numConstituents;
-    int numWaterbodies;
+    private List<String> constituentNames; // Constituent names
+    private List<List<String>> Data;     // State of each constituent (ON or OFF)
+    private int numConstituents;
+    private int numWaterbodies;
 
     public ActiveDerivedConstituentsCard(W2ControlFile w2ControlFile, int numConstituents, int numWaterbodies) {
-        super(w2ControlFile, "CST DERI", numConstituents);
+        super(w2ControlFile, "CST DERI",
+                (int) Math.ceil(numConstituents/9.0) * numConstituents);
         this.numConstituents = numConstituents;
         this.numWaterbodies = numWaterbodies;
         parseText();
@@ -44,20 +45,28 @@ public class ActiveDerivedConstituentsCard extends Card {
     public void parseText() {
         constituentNames = new ArrayList<>();
         Data = new ArrayList<>();
+        int numLinesPerConstituent = (int) Math.ceil(numConstituents/9.0);
+        int numLines = numLinesPerConstituent * numConstituents;
+
         for (int jwb = 0; jwb < numWaterbodies; jwb++) {
             Data.add(new ArrayList<>());
         }
 
+        int lineIndex = 0;
         for (int jc = 0; jc < numConstituents; jc++) {
-            List<String> Fields = parseLine(recordLines.get(jc), 8, 1, 10);
+            List<String> Fields = parseLine(recordLines.get(lineIndex),
+                   8, 1, 10);
             constituentNames.add(Fields.get(0));
+            int col = 1;
             for (int jwb = 0; jwb < numWaterbodies; jwb++) {
-                int col = jwb + 1;
-//                String str = Fields.get(col);
-//                List<String> wb = Data.get(jwb);
-//                wb.add(str);
+                if (col > 10) {
+                    col = 1;
+                    lineIndex++;
+                }
                 Data.get(jwb).add(Fields.get(col));
+                col++;
             }
+            lineIndex++;
         }
     }
 
@@ -66,7 +75,7 @@ public class ActiveDerivedConstituentsCard extends Card {
         for (int i = 0; i < numConstituents; i++) {
             String str = String.format("%-8s", constituentNames.get(i));
             for (int j = 0; j < numWaterbodies; j++) {
-                str += String.format("%%8s", Data.get(j).get(i));
+                str += String.format("%8s", Data.get(j).get(i));
                 recordLines.set(i, str);
             }
         }
